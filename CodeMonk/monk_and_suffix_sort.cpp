7b44214bfa38ab06a5a6abea3d8da873a2644e4e
Ajit Basarur <ajit.basarur@kit.edu>
2021-07-27 21:47:13 +0200

    Suffix sorting program

---
+++ CodeMonk/monk_and_suffix_sort.cpp
@@ -0,0 +1,43 @@
+/*
+Monk and Suffix Sort
+Monk loves to play games. On his birthday, his friend gifted him a string S. Monk and his friend started playing a new game called as Suffix Game. In Suffix Game, Monk's friend will ask him lexicographically  smallest suffix of the string S. Monk wants to eat the cake first so he asked you to play the game.
+
+Input Format:
+First line contains a string S () and an integer k ().
+
+Output Format:
+Print the lexicographically  smallest suffix of the string S.
+
+*/
+#include<iostream>
+#include<string>
+
+// This constraint is defined in the problem.
+#define MAX_STRING_LEN 25
+
+using namespace std;
+
+int main(){
+	
+	string S;
+	unsigned int N;
+
+	// Enter the string S
+	cin >> S;
+
+	int lenS = S.length();
+
+	if(lenA > MAX_STRING_LEN){
+		cout << "String length exceeds %d " << lenS;
+		return -1;
+	}
+	// Enter the length of the 
+	cin >> N;
+
+	if(N > lenS){
+		cout << "The order of the suffix exceeds the string length"
+	}
+
+
+	return 0:
+}
